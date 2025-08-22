bd293d60d5f318ce9d2905d789f87af1842a993c
mehmet-2023 <mefeocal455@gmail.com>
2025-08-22 13:30:05 +0300

    E

--- src/main/java/frc/robot/subsystems/SwerveTankSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveTankSubsystem.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.enums.ControlType;
+import com.revrobotics.spark.CANSparkMaxPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveTankSubsystem extends SubsystemBase {
@@ -12,24 +14,43 @@ public class SwerveTankSubsystem extends SubsystemBase {
     private final SparkMax blDrive = new SparkMax(7, MotorType.kBrushless);
     private final SparkMax brDrive = new SparkMax(5, MotorType.kBrushless);
 
-    private final SparkMax flStreer = new SparkMax(2, MotorType.kBrushless);
+    private final SparkMax flSteer = new SparkMax(2, MotorType.kBrushless);
     private final SparkMax frSteer = new SparkMax(4, MotorType.kBrushless);
     private final SparkMax blSteer = new SparkMax(8, MotorType.kBrushless);
-    private final SparkMax brSteer = new SparkMax(6, MotorType.kBrushless); 
+    private final SparkMax brSteer = new SparkMax(6, MotorType.kBrushless);
+
+    private final CANSparkMaxPIDController flPID = flSteer.getPIDController();
+    private final CANSparkMaxPIDController frPID = frSteer.getPIDController();
+    private final CANSparkMaxPIDController blPID = blSteer.getPIDController();
+    private final CANSparkMaxPIDController brPID = brSteer.getPIDController();
+
+    private final double flHome, frHome, blHome, brHome;
 
     public SwerveTankSubsystem() {
         SparkMaxConfig config = new SparkMaxConfig();
         config.idleMode(SparkBaseConfig.IdleMode.kBrake);
         config.inverted(false);
+        config.closedLoop.p(0.1).i(0).d(0).outputRange(-1, 1);
 
         flDrive.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         frDrive.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         blDrive.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         brDrive.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
-        flStreer.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+
+        flSteer.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         frSteer.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         blSteer.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         brSteer.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
+
+        flHome = flSteer.getEncoder().getPosition();
+        frHome = frSteer.getEncoder().getPosition();
+        blHome = blSteer.getEncoder().getPosition();
+        brHome = brSteer.getEncoder().getPosition();
+
+        flPID.setReference(flHome, ControlType.kPosition);
+        frPID.setReference(frHome, ControlType.kPosition);
+        blPID.setReference(blHome, ControlType.kPosition);
+        brPID.setReference(brHome, ControlType.kPosition);
     }
 
     public void setLeft(double power) {
@@ -46,5 +67,4 @@ public class SwerveTankSubsystem extends SubsystemBase {
         setLeft(0);
         setRight(0);
     }
-
 }
